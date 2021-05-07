57ed44560d861217edb27129f9e7aaea9cb2ec99
Parizval <anmolgoyal@gmail.com>
2021-05-07 15:45:46 +0530

    testing sidebar for UI

--- pages/index.vue
+++ pages/index.vue
@@ -1,157 +1,137 @@
+
 <template>
-  <div class="container">
-    <div>
-      <header class="content-logos">
-        <logo />
-        <span class="plus">+</span>
-        <VuesaxLogo />
-      </header>
-      <h1 class="title">
-        Nuxt.js + Vuesax
-      </h1>
-      <h2 class="subtitle">
-        <a href="https://vuesax.com/">Vuesax</a> is a framework of ui components for <a href="https://vuejs.org/">Vuejs</a>, It was created to make new interfaces that have a new trend and are visually beautiful
-      </h2>
-      <div class="links">
-        <h3 class="h3">
-          Vuesax
-        </h3>
-        <a
-          href="https://vuesax.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
+    <div class="hidden">
+      <vs-sidebar
+        absolute
+        hover-expand
+        reduce
+        v-model="active"
+        open
         >
-          Documentation
-        </a>
-        <a
-          href="https://discordapp.com/invite/9dsKtvB"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          Discord
-        </a>
-        <a
-          href="https://github.com/lusaxweb/vuesax"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-      <div class="links">
-        <h3 class="h3">
-          Nuxt.js
-        </h3>
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+        <template #logo>
+          <!-- ...img logo -->
+        </template>
+        <vs-sidebar-item id="home">
+          <template #icon>
+            <i class='bx bx-home'></i>
+          </template>
+          Home
+        </vs-sidebar-item>
+        <vs-sidebar-item id="market">
+          <template #icon>
+            <i class='bx bx-grid-alt'></i>
+          </template>
+          Market Overview
+        </vs-sidebar-item>
+        <vs-sidebar-item id="Music">
+          <template #icon>
+            <i class='bx bxs-music'></i>
+          </template>
+          Music
+        </vs-sidebar-item>
+        <vs-sidebar-group>
+          <template #header>
+            <vs-sidebar-item arrow>
+              <template #icon>
+                <i class='bx bx-group'></i>
+              </template>
+              Social media
+            </vs-sidebar-item>
+          </template>
+
+          <vs-sidebar-item id="Instagram">
+            <template #icon>
+              <i class='bx bxl-instagram'></i>
+            </template>
+            Instagram
+          </vs-sidebar-item>
+          <vs-sidebar-item id="twitter">
+            <template #icon>
+              <i class='bx bxl-twitter' ></i>
+            </template>
+            Twitter
+          </vs-sidebar-item>
+          <vs-sidebar-item id="Facebook">
+            <template #icon>
+              <i class='bx bxl-facebook' ></i>
+            </template>
+            Facebook
+          </vs-sidebar-item>
+        </vs-sidebar-group>
+        <vs-sidebar-group>
+          <template #header>
+            <vs-sidebar-item arrow>
+              <template #icon>
+                <i class='bx bx-code-alt' ></i>
+              </template>
+              Coding
+            </vs-sidebar-item>
+          </template>
+
+          <vs-sidebar-item id="github">
+            <template #icon>
+              <i class='bx bxl-github' ></i>
+            </template>
+            Github
+          </vs-sidebar-item>
+          <vs-sidebar-item id="codepen">
+            <template #icon>
+              <i class='bx bxl-codepen'></i>
+            </template>
+            Codepen
+          </vs-sidebar-item>
+          <vs-sidebar-item id="discord">
+            <template #icon>
+              <i class='bx bxl-discord'></i>
+            </template>
+            Discord
+          </vs-sidebar-item>
+          <vs-sidebar-item id="Javascript">
+            <template #icon>
+              <i class='bx bxl-javascript' ></i>
+            </template>
+            Javascript
+          </vs-sidebar-item>
+          <vs-sidebar-item id="git">
+            <template #icon>
+              <i class='bx bxl-git' ></i>
+            </template>
+            Git
+          </vs-sidebar-item>
+        </vs-sidebar-group>
+        <vs-sidebar-item id="donate">
+          <template #icon>
+            <i class='bx bxs-donate-heart' ></i>
+          </template>
+          Donate
+        </vs-sidebar-item>
+        <vs-sidebar-item id="drink">
+          <template #icon>
+            <i class='bx bx-drink'></i>
+          </template>
+          Drink
+        </vs-sidebar-item>
+        <vs-sidebar-item id="shopping">
+          <template #icon>
+            <i class='bx bxs-shopping-bags'></i>
+          </template>
+          Shopping
+        </vs-sidebar-item>
+        <vs-sidebar-item id="chat">
+          <template #icon>
+            <i class='bx bx-chat' ></i>
+          </template>
+          Chat
+        </vs-sidebar-item>
+      </vs-sidebar>
     </div>
-  </div>
-</template>
-
+  </template>
 <script>
-import Logo from '~/components/Logo.vue'
-import VuesaxLogo from '~/components/VuesaxLogo.vue'
-
-export default {
-  components: {
-    Logo,
-    VuesaxLogo
+  export default {
+    data:() => ({
+      active: 'home',
+    })
   }
-}
-</script>
+  </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 55px;
-  color: #35495e;
-  letter-spacing: 1px;
-  text-transform: capitalize;
-  margin: 25px 0;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 1.1rem;
-  color: #526488;
-  word-spacing: 2px;
-  padding-bottom: 15px;
-  max-width: 600px;
-}
-
-.subtitle a {
-  font-weight: 500;
-  color: inherit;
-}
-
-.links {
-  padding-top: 15px;
-  margin-bottom: 20px;
-}
-
-.content-logos {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-width: 500px;
-}
-
-.plus {
-  font-size: 2.5rem;
-  margin: 15px;
-  color: #35495e;
-}
-
-.h3 {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-weight: 400;
-  margin: 10px;
-}
-</style>
+        
